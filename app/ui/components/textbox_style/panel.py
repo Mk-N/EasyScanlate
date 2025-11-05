4d92893401d3aa61c7af7f713661cde1d6acd77d
Liiesl <gobloklo777@gmail.com>
2025-11-06 02:19:42 +0700

    fix import on panel

--- app/ui/components/textbox_style/panel.py
+++ app/ui/components/textbox_style/panel.py
@@ -6,7 +6,7 @@ from PySide6.QtWidgets import (QWidget, QVBoxLayout, QLabel, QPushButton, QFrame
 from PySide6.QtCore import Qt, Signal, QSettings
 from PySide6.QtGui import QColor
 import qtawesome as qta
-from app.ui.components import PresetButton
+from app.ui.components.textbox_style.preset import PresetButton
 # TEXT_BOX_STYLE_PANEL_STYLESHEET is no longer needed here
 from assets import DEFAULT_GRADIENT, TEXT_BOX_STYLE_PANEL_STYLE
 from app.ui.dialogs import CustomColorDialog
